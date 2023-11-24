7d40ea0b2dc389ead29fd6507967b954a6c97ab4
kzkito <kazzy2012@gmail.com>
2023-11-24 17:20:27 +0900

    t

--- 04_firebase/js/config.js
+++ 04_firebase/js/config.js
@@ -1,3 +1,8 @@
 const config = {
-   SETTINGS
+    apiKey: "AIzaSyAz-XfShn9S9PVODjB7EWpteuRn3cgd0eE",
+    authDomain: "gsdev0603-751b8.firebaseapp.com",
+    projectId: "gsdev0603-751b8",
+    storageBucket: "gsdev0603-751b8.appspot.com",
+    messagingSenderId: "848984406271",
+    appId: "1:848984406271:web:b2e46b758e24149ae2f4ca"
     }
