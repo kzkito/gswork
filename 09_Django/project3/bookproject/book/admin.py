7207683578245e36e2658efb6d5848cac7ec1b5c
kzkito <kazzy2012@gmail.com>
2024-01-05 21:38:58 +0900

    tes

--- 09_Django/project3/bookproject/book/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Book
-# from .models import SampleModel
-
-# Register your models here.
-# admin.site.register(SampleModel)
-admin.site.register(Book)
